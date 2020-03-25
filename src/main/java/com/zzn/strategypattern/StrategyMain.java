0359bf5b0bf12c1e426a6a6d0c85dbf822b12a6b
zzn <18729877606@163.com>
2020-03-25 22:03:57 +0800

    策略模式

--- src/main/java/com/zzn/strategypattern/StrategyMain.java
+++ src/main/java/com/zzn/strategypattern/StrategyMain.java
@@ -1,11 +1,13 @@
 package com.zzn.strategypattern;
 
+
 /**
  * @author zzn
  * @version 1.0 创建时间 2020/3/25 21:45
  */
 public class StrategyMain {
     public static void main(String[] args) {
+        //策略模式,参考 java.util.concurrent.TimeUnit
         System.out.println(Math.OperationAdd.doOperation(3, 2));
         System.out.println(Math.OperationSubtract.doOperation(3, 2));
         System.out.println(Math.OperationMultiple.doOperation(3, 2));
